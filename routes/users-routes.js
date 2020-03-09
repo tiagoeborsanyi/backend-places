0e7ebf7faeba0234caabb0140ba632f81728d917
Tiago Emerick <tiagoeborsanyi@gmail.com>
2020-03-09 11:06:25 -0300

    created user routes

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,3 +1,13 @@
-const teste = {
-  
-}
+const express = require('express');
+
+const usersController = require('../controllers/users-controllers');
+
+const router = express.Router();
+
+router.get('/', usersController.getUsers);
+
+router.post('/signup', usersController.signup);
+
+router.post('/login', usersController.login);
+
+module.exports = router;
